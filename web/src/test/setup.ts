1ad5ca10aef308cc863d089f29e6f90725168465
learngermanghana <learngermanghana@gmail.com>
2025-10-02 15:09:28 +0000

    Update Firebase env stubs for sedifex-ac2b0

--- web/src/test/setup.ts
+++ web/src/test/setup.ts
@@ -1,9 +1,9 @@
 import '@testing-library/jest-dom/vitest'
 
 vi.stubEnv('VITE_FB_API_KEY', 'test-api-key')
-vi.stubEnv('VITE_FB_AUTH_DOMAIN', 'sedifex.test.local')
-vi.stubEnv('VITE_FB_PROJECT_ID', 'sedifex-test')
-vi.stubEnv('VITE_FB_STORAGE_BUCKET', 'sedifex-test.appspot.com')
+vi.stubEnv('VITE_FB_AUTH_DOMAIN', 'sedifex-ac2b0.firebaseapp.com')
+vi.stubEnv('VITE_FB_PROJECT_ID', 'sedifex-ac2b0')
+vi.stubEnv('VITE_FB_STORAGE_BUCKET', 'sedifex-ac2b0.appspot.com')
 vi.stubEnv('VITE_FB_APP_ID', '1:1234567890:web:test')
 vi.stubEnv('VITE_FB_FUNCTIONS_REGION', 'us-central1')
 
