771898bc5830457924831b7954a0d1e1548e2894
learngermanghana <learngermanghana@gmail.com>
2025-10-03 20:19:48 +0000

    Expose supabase types

--- web/src/supabaseClient.ts
+++ web/src/supabaseClient.ts
@@ -1,6 +1,9 @@
-
-import { createClient } from '@supabase/supabase-js'
-
+import {
+  createClient,
+  type Session,
+  type SupabaseClient,
+  type User,
+} from '@supabase/supabase-js'
 import { supabaseEnv } from './config/supabaseEnv'
 
 export const supabase = createClient(supabaseEnv.url, supabaseEnv.anonKey, {
@@ -10,3 +13,4 @@ export const supabase = createClient(supabaseEnv.url, supabaseEnv.anonKey, {
   },
 })
 
+export type { Session, SupabaseClient, User }
