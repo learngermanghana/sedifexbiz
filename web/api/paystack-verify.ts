e4f256f2a5b2e46436d22a0b15197acd9ec4702a
learngermanghana <learngermanghana@gmail.com>
2025-10-07 14:32:04 +0000

    Update paystack-verify.ts

--- web/api/paystack-verify.ts
+++ web/api/paystack-verify.ts
@@ -1,5 +1,7 @@
 import type { VercelRequest, VercelResponse } from "@vercel/node";
-import { db } from "./_firebase-admin";
+// ⬇️ add ".js"
+import { db } from "./_firebase-admin.js";
+
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
   if (req.method !== "POST") return res.status(405).send("Method Not Allowed");
