11b6444231d7586a51c9647d613b8bdfe3215f91
learngermanghana <learngermanghana@gmail.com>
2025-10-23 11:13:26 +0000

    Remove product creation and edit pages

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -5,8 +5,6 @@ import { createBrowserRouter, RouterProvider, redirect } from 'react-router-dom'
 import App from './App'
 import Shell from './layout/Shell'
 import Products from './pages/Products'
-import ProductNew from './pages/ProductNew'
-import ProductEdit from './pages/ProductEdit'
 import { ToastProvider } from './components/ToastProvider'
 import { WorkspaceSelectProvider } from './hooks/useWorkspaceSelect'
 
@@ -18,10 +16,8 @@ const router = createBrowserRouter([
       // Default to products list
       { index: true, element: <Shell><Products /></Shell> },
 
-      // Product-only routes
+      // Product-only route
       { path: 'products', element: <Shell><Products /></Shell> },
-      { path: 'products/new', element: <Shell><ProductNew /></Shell> },
-      { path: 'products/:id', element: <Shell><ProductEdit /></Shell> },
     ],
   },
 
