a9629d0be93017f3733ffea0cba760f3445d22d9
learngermanghana <learngermanghana@gmail.com>
2025-11-09 16:29:19 +0000

    Delete python_functions directory

--- python_functions/constants.py
+++
@@ -1,4 +0,0 @@
-"""Constants ported from the TypeScript implementation."""
-INACTIVE_WORKSPACE_MESSAGE = (
-    "Your Sedifex workspace contract is not active. Reach out to your Sedifex administrator to restore access."
-)
