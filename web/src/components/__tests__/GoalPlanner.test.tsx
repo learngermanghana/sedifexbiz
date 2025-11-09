78d97fc86d6ccb4953e074efba8366cc1ef7b34a
learngermanghana <learngermanghana@gmail.com>
2025-11-09 18:09:38 +0000

    Fix GoalPlanner tests expect type

--- web/src/components/__tests__/GoalPlanner.test.tsx
+++ web/src/components/__tests__/GoalPlanner.test.tsx
@@ -1,7 +1,7 @@
 import { render, screen, waitFor } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import { MemoryRouter } from 'react-router-dom'
-import { vi } from 'vitest'
+import { expect, vi } from 'vitest'
 
 import GoalPlanner from '../GoalPlanner'
 import Shell from '../../layout/Shell'
