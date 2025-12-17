e5700369b5422f7e15895ea68e2ea7c8b795a498
learngermanghana <learngermanghana@gmail.com>
2025-12-17 10:32:14 +0000

    Delete functions directory

--- functions/src/pos/prepareReceiptShare.ts
+++
@@ -1,4 +0,0 @@
-import * as functions from 'firebase-functions';
-export const prepareReceiptShare = functions.https.onCall(async () => {
-  return { ok: false, reason: 'prepareReceiptShare not implemented yet' };
-});
--- functions/src/pos/receiveStock.ts
+++
@@ -1,4 +0,0 @@
-import * as functions from 'firebase-functions';
-export const receiveStock = functions.https.onCall(async () => {
-  return { ok: false, reason: 'receiveStock not implemented yet' };
-});
