7407b55500786e874be9c825da3db0c5858c894d
learngermanghana <learngermanghana@gmail.com>
2025-12-30 11:45:25 +0000

    Delete functions directory

--- functions/lib/constants/access.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-// functions/src/constants/access.ts
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.INACTIVE_WORKSPACE_MESSAGE = void 0;
-exports.INACTIVE_WORKSPACE_MESSAGE = 'Your Sedifex workspace is inactive. Please contact the workspace owner or Sedifex support to reactivate it.';
--- functions/src/constants/access.ts
+++
@@ -1,4 +0,0 @@
-// functions/src/constants/access.ts
-
-export const INACTIVE_WORKSPACE_MESSAGE =
-  'Your Sedifex workspace is inactive. Please contact the workspace owner or Sedifex support to reactivate it.'
