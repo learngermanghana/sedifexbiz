9f7affeeb6f74a7812e70628983808f73019502c
learngermanghana <learngermanghana@gmail.com>
2025-12-31 20:17:20 +0000

    Delete functions directory

--- functions/src/pos/manageStaffAccount.ts
+++
@@ -1,4 +0,0 @@
-import * as functions from 'firebase-functions';
-export const manageStaffAccount = functions.https.onCall(async () => {
-  return { ok: false, reason: 'manageStaffAccount not implemented yet' };
-});
