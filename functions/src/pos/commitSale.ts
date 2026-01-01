2c79ffb2059b093a0b493b745e31c70fa911c768
learngermanghana <learngermanghana@gmail.com>
2026-01-01 03:37:58 +0000

    Delete functions directory

--- functions/src/pos/commitSale.ts
+++
@@ -1,5 +0,0 @@
-import * as functions from 'firebase-functions';
-
-export const commitSale = functions.https.onCall(async () => {
-  return { ok: false, reason: 'commitSale not implemented yet' };
-});
