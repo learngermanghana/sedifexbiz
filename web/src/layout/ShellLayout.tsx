2fa71c94ef4f19faf0b151e4fd7f8cb15c788ae0
learngermanghana <learngermanghana@gmail.com>
2026-08-27 07:06:16 -0700

    Keep a single PDF export dock mount

--- web/src/layout/ShellLayout.tsx
+++ web/src/layout/ShellLayout.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 import { Outlet } from 'react-router-dom'
 import AskSedifexAgent from '../components/AskSedifexAgent'
 import EventClientCollaborationDock from '../components/EventClientCollaborationDock'
-import EventPdfExportDock from '../components/EventPdfExportDock'
 import Shell from './Shell'
 
 const SHOW_ASK_SEDIFEX = false
@@ -12,7 +11,7 @@ export function ShellLayout() {
     <Shell>
       <Outlet />
       <EventClientCollaborationDock />
-      <EventPdfExportDock />
+      {/* EventPdfExportDock is mounted once at App level so list and workspace routes share one control. */}
       {/* Temporarily hide Ask Sedifex until it is ready to return. */}
       {SHOW_ASK_SEDIFEX ? <AskSedifexAgent enabled /> : null}
     </Shell>
