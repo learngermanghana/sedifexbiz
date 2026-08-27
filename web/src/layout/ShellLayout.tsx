e6d406363b21b9ebddc5a84238cb2b858312f936
learngermanghana <learngermanghana@gmail.com>
2026-08-27 09:11:51 -0700

    Show event email sender on event planning pages

--- web/src/layout/ShellLayout.tsx
+++ web/src/layout/ShellLayout.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Outlet } from 'react-router-dom'
 import AskSedifexAgent from '../components/AskSedifexAgent'
 import EventClientCollaborationDock from '../components/EventClientCollaborationDock'
+import EventEmailDeliveryDock from '../components/EventEmailDeliveryDock'
 import Shell from './Shell'
 
 const SHOW_ASK_SEDIFEX = false
@@ -11,6 +12,7 @@ export function ShellLayout() {
     <Shell>
       <Outlet />
       <EventClientCollaborationDock />
+      <EventEmailDeliveryDock />
       {/* Keep EventPdfExportDock mounted only at App level so the Events list and full event workspace share one export control. */}
       {/* Temporarily hide Ask Sedifex until it is ready to return. */}
       {SHOW_ASK_SEDIFEX ? <AskSedifexAgent enabled /> : null}
